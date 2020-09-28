2c5daf702e75f613ba87d6729991f4b9fc9161c4
KiranAnand-Peach <57107242+KiranAnand-Peach@users.noreply.github.com>
2020-09-28 14:06:37 +0100

    Error Handling

--- readJson.php
+++ readJson.php
@@ -1,11 +1,24 @@
 <?php
 
-    // Use second argument to specify if an aray is returned instead of an object i.e. use $emp['name'] instead of $emp->name
-    $emps = json_decode($_POST['emps'], false);
+    $str = $_POST['emps'];
 
-    foreach($emps as $emp)
+    // Simulate an error
+    $str = '[{"id":1,"name":"Kiran","address":"48 Rowan Court"},{"id":2,"name":"Derek","address":"48 Rowan Court"}]'; // Correct
+    $str = "[{'id':1,'name':'Kiran','address':'48 Rowan Court'},{'id':2,'name':'Derek','address':'48 Rowan Court'}]"; // Incorrect
+
+    // Use second argument to specify if an aray is returned instead of an object i.e. use $emp['name'] instead of $emp->name
+    $emps = json_decode($str, false);
+
+    if(json_last_error() == JSON_ERROR_NONE)
     {
-        print_r($emp->name . '<br>');
+        foreach($emps as $emp)
+        {
+            print_r($emp->name . '<br>');
+        }
+    }
+    else
+    {
+        echo json_last_error_msg();
     }
 
 ?>
