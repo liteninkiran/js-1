c3390680ac6d87b442190d314dd5a8603b2afd6f
KiranAnand-Peach <57107242+KiranAnand-Peach@users.noreply.github.com>
2020-09-28 10:40:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSON in PHP And JavaScript</title>
+        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    </head>
+    <body>
+        <center>
+            <h1>JSON in PHP and JavaScirpt</h1>
+            <h2>Part 3: JSON.stringify and json_decode</h2>
+        </center>
+    </body>
+    <script type="text/javascript">
+        
+    </script>
+</html>
